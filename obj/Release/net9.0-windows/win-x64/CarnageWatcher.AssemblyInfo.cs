23d17fa82e691a5aab34fad4c18c9c289fb0761c
placeholder <julianabaroa38@gmail.com>
2025-08-14 14:34:25 -0400

    fix (csproj): added appsettings

--- obj/Release/net9.0-windows/win-x64/CarnageWatcher.AssemblyInfo.cs
+++ obj/Release/net9.0-windows/win-x64/CarnageWatcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarnageWatcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29487389f042aac2849af2db162cafbec27d6dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0080d068e916fb0fa52483758900f97955a1175")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarnageWatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarnageWatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
